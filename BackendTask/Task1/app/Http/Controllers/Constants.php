4824eb5c18944777cff49bd7ad5764a9fd09b641
AhmedHumk <77048689+AhmedHumk@users.noreply.github.com>
2023-08-26 10:11:23 +0300

    Update Constants.php

--- BackendTask/Task1/app/Http/Controllers/Constants.php
+++ BackendTask/Task1/app/Http/Controllers/Constants.php
@@ -7,4 +7,5 @@ use Illuminate\Http\Request;
 class Constants extends Controller
 {
     const SiteUrl = "https://www.kotobati.com/section/%D8%B1%D9%88%D8%A7%D9%8A%D8%A7%D8%AA";
+    const BaseUrl = "https://www.kotobati.com";
 }

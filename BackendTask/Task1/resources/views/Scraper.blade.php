b9bfd10f81d4c39477ac3c494bfda8e6e5405ebd
AhmedHumk <77048689+AhmedHumk@users.noreply.github.com>
2023-08-26 10:13:56 +0300

    adding a view details button button to show more information about the selected book.

--- BackendTask/Task1/resources/views/Scraper.blade.php
+++ BackendTask/Task1/resources/views/Scraper.blade.php
@@ -10,13 +10,17 @@
 <h1>Results</h1>
 <div id="resultsContainer">
         <h2>Scraped Results:</h2>
+        <div id="book-details-container"></div> 
+        <br>
+        <button type="button" id="load-more-button" data-page="{{ $pageUrl }}" onclick="loadMore()">Load More</button>
         <ul>
             @foreach ($results as $result)
                 <li>Book Title: {{ $result['title'] }}</li>
                 <li>Book Author: {{ $result['author'] }}</li>
+                <li><button type="button" onclick="getBookDetails(this)" data-bookurl="{{ $result['bookurl'] }}">View Details</button></li>
+                <hr>
             @endforeach
         </ul>
-        <button type="button" id="load-more-button" data-page="{{ $pageUrl }}" onclick="loadMore()">Load More</button>
     </div>
 </body>
 </html>
